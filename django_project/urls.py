56e9b7b987de12c7b904aea7922a669da4ee8dfe
abdo-mo <abdo.mo.py3@gmail.com>
2021-03-19 19:18:03 +0200

    reset password feature

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,6 +21,9 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/',
         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
         name = "password_reset_confirm"), 
+    path('password-reset_complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        name = "password_reset_complete"),
     path('', include('blog.urls')),
 ]
 
