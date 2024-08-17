60efcad23e52fdb7f59aba2656fd52cfbb04eb10
abdo-mo <abdo.mo.py3@gmail.com>
2024-08-17 22:26:47 +0300

    fix error in admin file

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post
 
 admin.site.register(Post)
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'post', 'created', 'active')
-    list_filter = ('active', 'created', 'updated')
-    search_fields = ('name', 'email', 'body')
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'email', 'post', 'created', 'active')
+#     list_filter = ('active', 'created', 'updated')
+#     search_fields = ('name', 'email', 'body')
